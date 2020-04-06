774eb7f2da8474ecec28cacdd22e2fef7b9e04c7
Mohammed Bettaoui <bettapro100@gmail.com>
2020-04-06 14:25:01 +0100

    Progress

---
+++ js/Artists.js
@@ -0,0 +1,28 @@
+const Artists = [
+  {
+    id: 1,
+    name: 'john Doe',
+    email: 'john@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Bob Williams',
+    email: 'bob@gmail.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Shannon Jackson',
+    email: 'shannon@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 4,
+    name: 'Bob Marley',
+    email: 'Bob@gmail.com',
+    status: 'active'
+  }
+];
+
+module.exports = Artists;
--- js/server.js
+++ js/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const path = require('path');
+const Artists = require('./Artists')
 
 const app = express();
 const PORT = process.env.PORT || 2020;
 
+const logger = (req, res, next) => {
+  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+  next();
+}
+// Init middelware
+app.use(logger);
 
+//Getts All Membres //
+app.get('/api/Artists', (req, res) => {res.json(Artists)});
+
+// Set Static Folder //
 app.use(express.static(path.join(__dirname, '../public')));
 // Get Methode //
 //app.get('/', (req, res) =>{
